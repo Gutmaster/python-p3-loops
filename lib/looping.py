27088c876abf9540f0c43e4211c6e22d9e5ea10d
+ <+>
2024-05-11 09:13:06 -0700

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10):
+        print(10 - i)
+        if i == 9:
+            print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i*i for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        if (i+1) % 3 == 0 and (i+1) % 5 == 0:
+            print("FizzBuzz")
+        elif (i+1) % 3 == 0:
+            print("Fizz")
+        elif (i+1) % 5 == 0:
+            print("Buzz")
+        else:
+            print(i+1)
